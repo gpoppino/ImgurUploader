07f0e97a88711c0da7685b11546a08c90108dbf3
Geronimo Poppino <gpoppino@outlook.com>
2022-11-17 15:12:37 -0300

    Make function names more pythonic

--- uploader.py
+++ uploader.py
@@ -10,17 +10,19 @@ AUTHORIZATION_URL = "https://api.imgur.com/oauth2/authorize"
 TOKEN_URL = "https://api.imgur.com/oauth2/token"
 RESPONSE_TYPE = "token"
 
+
 class ImgurAuthorizer:
 
     def __init__(self):
         self.__config = configparser.ConfigParser()
         self.__config.read('.client_secrets')
 
-    def authorizeClient(self):
-        url = AUTHORIZATION_URL + "?client_id=%s&response_type=%s" % (self.__config['credentials']['client_id'], RESPONSE_TYPE)
+    def authorize_client(self):
+        url = AUTHORIZATION_URL + "?client_id=%s&response_type=%s" % (
+            self.__config['credentials']['client_id'], RESPONSE_TYPE)
         webbrowser.open(url)
 
-    def getNewRefreshToken(self):
+    def get_new_refresh_token(self):
         url = TOKEN_URL
         payload = {
             'refresh_token': self.__config['credentials']['refresh_token'],
@@ -40,15 +42,15 @@ class ImgurAuthorizer:
 
         return response.json()['access_token']
 
-    def isAuthorized(self):
+    def is_authorized(self):
         return self.__config['credentials']['access_token'] != ""
 
-    def updateTokens(self, access_token, refresh_token):
+    def update_tokens(self, access_token, refresh_token):
         self.__config['credentials']['access_token'] = access_token
         self.__config['credentials']['refresh_token'] = refresh_token
         self.__config.write()
 
-    def getAccessToken(self):
+    def get_access_token(self):
         return self.__config['credentials']['access_token']
 
 
@@ -58,25 +60,27 @@ class ImgurUploader:
         self.__bar = None
 
     def __create_callback(self, encoder, filename):
-        widgets = [' ', progressbar.Percentage(),  ' ', progressbar.SimpleProgress()]
-        self.__bar = progressbar.ProgressBar(widgets=widgets, max_value=encoder.len, prefix="Uploading " + filename).start()
+        widgets = [' ', progressbar.Percentage(), ' ', progressbar.SimpleProgress()]
+        self.__bar = progressbar.ProgressBar(widgets=widgets, max_value=encoder.len,
+                                             prefix="Uploading " + filename).start()
 
         def callback(monitor):
             self.__bar.update(monitor.bytes_read)
 
         return callback
 
-    def __finishUpload(self):
+    def __finish_upload(self):
         self.__bar.finish()
 
-    def __buildHeaders(self, access_token, content_type):
+    @staticmethod
+    def __build_headers(my_access_token, content_type):
         headers = {
-            'Authorization': 'Bearer %s' % access_token,
+            'Authorization': 'Bearer %s' % my_access_token,
             'Content-Type': content_type
         }
         return headers
 
-    def uploadImage(self, imgurAuthorizer, image_path):
+    def upload_image(self, imgurAuthorizer, image_path):
         url = "https://api.imgur.com/3/image"
 
         with open(image_path, 'rb') as image_file:
@@ -91,16 +95,16 @@ class ImgurUploader:
             callback = self.__create_callback(encoder, image_path)
             monitor = MultipartEncoderMonitor(encoder, callback=callback)
 
-            headers = self.__buildHeaders(imgurAuthorizer.getAccessToken(), monitor.content_type)
+            headers = self.__build_headers(imgurAuthorizer.get_access_token(), monitor.content_type)
             response = requests.post(url, headers=headers, data=monitor, files=[])
             if response.status_code == 403:
                 new_access_token = imgurAuthorizer.getNewAccessToken()
-                if new_access_token == None:
+                if new_access_token is None:
                     return
-                headers = self.__buildHeaders(new_access_token, monitor.content_type)
+                headers = self.__build_headers(new_access_token, monitor.content_type)
                 response = requests.post(url, headers=headers, data=monitor, files=[])
 
-            self.__finishUpload()
+            self.__finish_upload()
 
             if response.status_code != 200:
                 print("Error: " + response.json()['data']['error'])
@@ -108,20 +112,21 @@ class ImgurUploader:
 
             return response.json()['data']
 
+
 if __name__ == '__main__':
     imgurAuthorizer = ImgurAuthorizer()
 
-    if not imgurAuthorizer.isAuthorized():
-        imgurAuthorizer.authorizeClient()
+    if not imgurAuthorizer.is_authorized():
+        imgurAuthorizer.authorize_client()
 
         access_token = input("Access Token:")
         refresh_token = input("Refresh Token:")
 
-        imgurAuthorizer.updateTokens(access_token, refresh_token)
+        imgurAuthorizer.update_tokens(access_token, refresh_token)
 
     if len(sys.argv) > 1:
         imgurUploader = ImgurUploader()
         for image in sys.argv[1:]:
-            data = imgurUploader.uploadImage(imgurAuthorizer, image)
+            data = imgurUploader.upload_image(imgurAuthorizer, image)
             if data is not None:
                 print("Done %s => %s" % (image, data['link']))
